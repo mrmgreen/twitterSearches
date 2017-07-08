067f842d9c9d20f1f109a554769cbbb00b406e20
mrmgreen <m.r.green@hotmail.co.uk>
2017-07-08 07:02:22 +0100

    creating a bar chart from static data

--- client/index.js
+++ client/index.js
@@ -1,7 +1,10 @@
 const barChart = () => {
-  const dataset = [ 5, 10, 14 ];
+  const dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+                11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
   const w = 500;
   const h = 100;
+  const barHeight = 100;
+  const barPadding = 1;
 
 const svg = d3.select('#root')
     .append('svg')
@@ -12,10 +15,19 @@ const svg = d3.select('#root')
     .data(dataset)
     .enter()
     .append('rect')
-    .attr('x', 0)
-    .attr('y', 0)
-    .attr('width', 20)
-    .attr('height', 100);
+    .attr('x', function(d, i) {
+      return i * (w / dataset.length);
+    })
+    .attr('y', function(d, i) {
+      return h - d * 4;
+    })
+    .attr('width', w / dataset.length - barPadding)
+    .attr('height', function(d) {
+      return d * 4;
+    })
+    .attr("fill", function(d) {
+      return "rgb(0, 0, " + (d * 10) + ")";
+    });
 };
 
 barChart();
