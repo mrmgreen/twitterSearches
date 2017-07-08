227680c87d50fd9f43f77a8846169a5bd05d83d2
mrmgreen <m.r.green@hotmail.co.uk>
2017-07-08 09:13:34 +0100

    scatter plot - scaling

--- client/index.js
+++ client/index.js
@@ -36,6 +36,7 @@ const svg = d3.select('#barChart')
 };
 
 function scatterPlot() {
+  const padding = 20;
   const dataset = [
                   [ 5,     20 ],
                   [ 480,   90 ],
@@ -49,6 +50,18 @@ function scatterPlot() {
                   [ 220,   88 ]
               ];
 
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset, d => d[0])])
+    .range([padding, w - padding]);
+
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset, d => d[1])])
+    .range([h - padding, padding]);
+
+  const rScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset, d => d[1])])
+    .range([2, 5]);
+
   const svg = d3.select('#scatterPlot')
   .append('svg')
   .attr('width', w)
@@ -58,17 +71,17 @@ function scatterPlot() {
     .data(dataset)
     .enter()
     .append('circle')
-    .attr('cx', d => d[0])
-    .attr('cy', d => d[1])
-    .attr('r', d => Math.sqrt(h - d[1]));
+    .attr('cx', d => xScale(d[0]))
+    .attr('cy', d => yScale(d[1]))
+    .attr('r', d => rScale(d[1]));
 
   svg.selectAll('text')
       .data(dataset)
       .enter()
       .append('text')
       .text(d => d[0] + "," + d[1])
-      .attr('x', d => d[0])
-      .attr('y', d => d[1])
+      .attr('x', d => xScale(d[0]))
+      .attr('y', d => yScale(d[1]))
       .attr('font-family', 'sans-serif')
       .attr('font-size', '11px')
       .attr('fill', 'red');
